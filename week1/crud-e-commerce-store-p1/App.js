a50b38f901cb09bea87d8cae509f2da72862b06c
claudiacole96 <claudia.evans@bryanuniversity.edu>
2021-06-27 15:58:17 -0400

    week3

---
+++ week1/crud-e-commerce-store-p1/App.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+mongoose.connect("mongodb://localhost:27017/moviedb",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => console.log("Connected to the DB")
+)
+
+app.listen(3000, () => {
+    console.log("The app is listening on port 3000")
+})
